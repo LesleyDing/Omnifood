aae4b31a6b6316f55408e18e0f1948137f553aae
Hanzhi Ding <leslietdhz2014@gmail.com>
2017-03-28 15:08:38 -0700

    added scrolling animate

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     
+    /* For the sticky navigation */
     $('.js--section-features').waypoint(function (direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -7,14 +8,33 @@ $(document).ready(function () {
             $('nav').removeClass('sticky');
         }
     }, {
-      offset: '60px'
-});
-/*
-var waypoints = $('#handler-first').waypoint(function(direction) {
-  notify(this.element.id + ' hit 25% from top of window') 
-}, {
-  offset: '25%'
-})
-*/
+        offset: '60px'
+    });
 
+    /* Scrolling to the button */
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+    
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+    
+    /* Navigation scroll */
+    $(function () {
+      $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+    
+    
 });
