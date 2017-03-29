a6aded508fdb76c091921345702a6db11217e467
Hanzhi Ding <leslietdhz2014@gmail.com>
2017-03-29 15:04:00 -0700

    added animations

--- resources/js/script.js
+++ resources/js/script.js
@@ -36,5 +36,30 @@ $(document).ready(function () {
       });
     });
     
+    /* Animation on scroll */
+    $('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    },{
+        offset: '50%'
+    });
+    
+    $('.js--wp-2').waypoint(function (direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    },{
+        offset: '50%'
+    });
+    
+    $('.js--wp-3').waypoint(function (direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    },{
+        offset: '50%'
+    });
+    
+    $('.js--wp-4').waypoint(function (direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    },{
+        offset: '50%'
+    });
+    
     
 });
